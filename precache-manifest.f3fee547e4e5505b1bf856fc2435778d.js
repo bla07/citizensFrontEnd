f0fedec0fc28db3a6a8c3f8bfc2fed0d4aabb881
Joe Phang Rahmansyah <joe18lg@gmail.com>
2020-08-14 23:37:00 +0700

    Updates

---
+++ precache-manifest.f3fee547e4e5505b1bf856fc2435778d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99e6c215732d66d1b287e8d26827099",
+    "url": "/citizensFrontEnd/index.html"
+  },
+  {
+    "revision": "38c835dec41010ddd821",
+    "url": "/citizensFrontEnd/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "ce80b5c732081d31b0e9",
+    "url": "/citizensFrontEnd/static/css/main.46413286.chunk.css"
+  },
+  {
+    "revision": "38c835dec41010ddd821",
+    "url": "/citizensFrontEnd/static/js/2.b2997c1e.chunk.js"
+  },
+  {
+    "revision": "067b735099407763a9b1e9f0b729b5ea",
+    "url": "/citizensFrontEnd/static/js/2.b2997c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce80b5c732081d31b0e9",
+    "url": "/citizensFrontEnd/static/js/main.047cb576.chunk.js"
+  },
+  {
+    "revision": "b805ae634261a85cbe6d",
+    "url": "/citizensFrontEnd/static/js/runtime-main.e19c4be0.js"
+  },
+  {
+    "revision": "86762bdbe5eef2058e335d4029520a38",
+    "url": "/citizensFrontEnd/static/media/1.86762bdb.jpg"
+  },
+  {
+    "revision": "39476b5136ada0db482da5419f1936b0",
+    "url": "/citizensFrontEnd/static/media/19462.39476b51.jpg"
+  },
+  {
+    "revision": "1ea159c0bb741c42e700e6aa804206dd",
+    "url": "/citizensFrontEnd/static/media/2.1ea159c0.jpg"
+  },
+  {
+    "revision": "a9633f64d8bda0b935ce0f71c28a1bb8",
+    "url": "/citizensFrontEnd/static/media/3.a9633f64.jpg"
+  },
+  {
+    "revision": "280c4afbc3d9b2025a45863cd1ab31d3",
+    "url": "/citizensFrontEnd/static/media/388516-PCBN6A-438.280c4afb.png"
+  },
+  {
+    "revision": "9b1f9d40d190c7209482bcbf90c7d085",
+    "url": "/citizensFrontEnd/static/media/4.9b1f9d40.jpg"
+  },
+  {
+    "revision": "630b70489dcef0e1d8b9dc07e4b65f4b",
+    "url": "/citizensFrontEnd/static/media/5.630b7048.jpg"
+  },
+  {
+    "revision": "eb58c9dfcc985f9bcfbaeeefcaec9ede",
+    "url": "/citizensFrontEnd/static/media/59981.eb58c9df.png"
+  },
+  {
+    "revision": "2a9670140d41d3de9bb7d3406187d58a",
+    "url": "/citizensFrontEnd/static/media/6.2a967014.jpg"
+  },
+  {
+    "revision": "f2d2dd55d46c53a64c02c3e98f6dbf86",
+    "url": "/citizensFrontEnd/static/media/6323.f2d2dd55.png"
+  },
+  {
+    "revision": "8446fd101b96d493cf8967a05f306d3f",
+    "url": "/citizensFrontEnd/static/media/Commentnews.8446fd10.svg"
+  },
+  {
+    "revision": "8d203d7decfd3f1a7bf595d4c126de58",
+    "url": "/citizensFrontEnd/static/media/Readnews.8d203d7d.svg"
+  },
+  {
+    "revision": "41f435fda8c9b0c89a14828e3248a191",
+    "url": "/citizensFrontEnd/static/media/Uploadnews.41f435fd.svg"
+  },
+  {
+    "revision": "4e5b40fc3be6928ccb308c2b4a4f142d",
+    "url": "/citizensFrontEnd/static/media/about.4e5b40fc.mp4"
+  }
+]);
